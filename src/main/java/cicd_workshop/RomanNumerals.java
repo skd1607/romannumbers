8cfd0917c528bdf1133286c100216c8f8cb62be6
skd1607 <37947915+skd1607@users.noreply.github.com>
2018-03-31 15:30:15 +0530

    kk
    kkk

--- src/main/java/cicd_workshop/RomanNumerals.java
+++ src/main/java/cicd_workshop/RomanNumerals.java
@@ -3,8 +3,13 @@ package cicd_workshop;
 public class RomanNumerals {
 
 	public String convert(int inputNumber) {
-		// TODO Auto-generated method stub
+		if (inputNumber == 1)
+		{
 		return "I";
+		}else if(inputNumber == 2) {
+		return "II";
+		}
+		
+		return "Not implemented";
+		
 	}
-
-}
